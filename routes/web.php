cbca3ecd39d3b7635b94919e30e7651a6389f8f2
vladagui2001 <vlad.aguiluz5c@gmail.com>
2022-11-05 11:58:23 -0600

    new file:   .env 	modified:   .gitignore 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mostraruser',compact('User'));
 });
 Route::resource("Users","DifUserController");
 Route::resource("RegUser","DifUserAdminController");
