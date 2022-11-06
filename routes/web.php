a87855cbbe251c52c120fa595c800d5b0b36b9ca
vladagui2001 <vlad.aguiluz5c@gmail.com>
2022-11-06 09:04:18 -0600

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome',compact('User'));
+    return view('welcome');
 });
 Route::resource("Users","DifUserController");
 Route::resource("RegUser","DifUserAdminController");
