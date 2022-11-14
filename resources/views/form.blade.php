6bfb59582b26279e6e1e6284a5739bf8414bb86e
vladagui2001 <vlad.aguiluz5c@gmail.com>
2022-11-14 12:29:24 -0600

    modified:   app/Http/Controllers/DifUserController.php 	new file:   resources/views/create2.blade.php 	new file:   resources/views/form.blade.php

---
+++ resources/views/form.blade.php
@@ -0,0 +1,22 @@
+
+<div class="form-group">
+    {{Form::label('Nombre','Nombre')}}
+    {{Form::text('Nombre',null,['class'=>'form-control'])}}
+    {{Form::label('Curp','Curp')}}
+    {{Form::text('Curp',null,['class'=>'form-control'])}}
+    {{Form::label('Genero','Genero')}}
+    {{Form::text('Genero',null,['class'=>'form-control'])}}
+    {{Form::label('NoDeHijos','NoDeHijos')}}
+    {{Form::text('NoDeHijos',null,['class'=>'form-control'])}}
+    {{Form::label('Discap','Discap')}}
+    {{Form::text('Discap',null,['class'=>'form-control'])}}
+    {{Form::label('Nomdiscap','Nomdiscap')}}
+    {{Form::text('Nomdiscap',null,['class'=>'form-control'])}}
+    {{Form::label('Ing_Men','Ing_Men')}}
+    {{Form::text('Ing_Men',null,['class'=>'form-control'])}}
+    {{Form::label('Nom_Apoyo','Nom_Apoyo')}}
+    {{Form::text('Nom_Apoyo',null,['class'=>'form-control'])}}
+    {{Form::label('Nom_Apoyo','Nom_Apoyo')}}
+    {{Form::text('Nom_Apoyo',null,['class'=>'form-control'])}}
+</div>
+
